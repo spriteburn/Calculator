ab8b9735d2f4d7c16612be31727b9749dec2b14f
andrew.f.trobec <andrew.f.trobec@accenture.com>
2014-03-31 12:22:08 +0200

    initial commit

---
+++ Test/calculator/TestCalculator.java
@@ -0,0 +1,24 @@
+package calculator;
+
+import junit.framework.TestCase;
+
+public class TestCalculator extends TestCase {
+	Calculator calc;
+
+	public void testAdd() {
+		assertEquals("testing add(3,4)", 7, calc.add(3, 4));
+	}
+
+	public void testMultiply() {
+		assertEquals("testing mult(0,4)", 0, calc.multiply(0, 4));
+	}
+
+	// public void testSubtract() {
+	// assertEquals("testing mult(5,4)", 1, calc.subtract(5, 4));
+	// }
+
+	public void setUp() {
+		calc = new Calculator();
+	}
+
+}
---
+++ src/calculator/Calculator.java
@@ -0,0 +1,17 @@
+package calculator;
+
+public class Calculator {
+
+	public int add(int x, int y) {
+		return x + y;
+	}
+
+	public int multiply(int x, int y) {
+		return x * y;
+	}
+
+	public void subtract(int x, int y) {
+		// return x * y;
+	}
+
+}
